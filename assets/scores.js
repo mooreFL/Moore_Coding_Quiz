396324285f8a958b009283574fa10d3fa09e1e2b
mooreFL <moorepr09@gmail.com>
2021-04-01 10:06:02 -0400

    fixed highscores array

--- assets/scores.js
+++ assets/scores.js
@@ -1,7 +1,23 @@
-var finalscore = localStorage.getItem("scores");
-var highscore = document.getElementById("pointsbox");
-highscore.append(finalscore);
+//parse the data from the database as a object so we can drill into each property
+var scores = JSON.parse(localStorage.getItem("scores"));
+var highscoreEl = document.getElementById("pointsbox");
+//for loop over array, create element for initial & score, 
 
 
+
+for (let index = 0; index < scores.length; index++) {
+    //create elements for the users scores
+    var initials = document.createElement("p")
+    var score = document.createElement("p")
+    //add the score and initials for each element
+    initials.textContent =  scores[index].initials;
+    score.textContent =  scores[index].score;
+    // add them to the page
+    highscoreEl.append(initials);
+    highscoreEl.append(score);
+    //create div or ul with li
+
+    
+}
 // "finalscore", JSON.stringify
 
